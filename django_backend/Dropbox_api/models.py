eddd532db9c15a2e05b00e77c3ce59db5a15923e
komaljit <komaljit1874@gmail.com>
2018-05-03 01:38:04 -0700

    redefining models

--- django_backend/Dropbox_api/models.py
+++ django_backend/Dropbox_api/models.py
@@ -2,18 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+# Folder model for virtual folders
+class Folder(models.Model):
+    folder_path = models.CharField(max_length=300, primary_key=True)
+    # foldername = models.CharField(max_length=20)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    parent_folder = models.CharField(max_length=300, default=username)
+
+
 # File model for the class
 class File(models.Model):
+    file_id = models.AutoField(primary_key=True)
     filename = models.CharField(max_length=50)
     file = models.FileField()
+    folder_path = models.ForeignKey(Folder, on_delete = models.CASCADE)
     username = models.ForeignKey(User, on_delete = models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
 
 
-class folder(models.Model):
-    foldername = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
+# model for shared file groups
 class Group(models.Model):
     group_id = models.AutoField(primary_key=True)
     group_name = models.CharField(max_length= 100)
